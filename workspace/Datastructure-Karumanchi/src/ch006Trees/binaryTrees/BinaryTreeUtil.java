b0b9e6570b7229f16bebf93176135024749c260d
Ankit Anand <ankitanand1212@gmail.com>
2020-08-02 10:44:53 +0530

    Binary Tree

--- workspace/Datastructure-Karumanchi/src/ch006Trees/binaryTrees/BinaryTreeUtil.java
+++ workspace/Datastructure-Karumanchi/src/ch006Trees/binaryTrees/BinaryTreeUtil.java
@@ -3,7 +3,6 @@ package ch006Trees.binaryTrees;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -123,6 +122,93 @@ public class BinaryTreeUtil {
 		root.left.right = new BinaryTreeNode(5);
 		List<Integer> rs = levelOrderTraversal(root);
 		System.out.println(rs);
+		System.out.println(minimumHeightViaRecursion(root));
 	}
+	
+	public static int sizeViaRecursion(BinaryTreeNode root) {
+		int lSize = root.left == null ? 0 : sizeViaRecursion(root.left);
+		int rSize = root.right == null ? 0 : sizeViaRecursion(root.right);
+		return lSize + rSize + 1;
+	}
+	
+	public static int sizeViaIteration(BinaryTreeNode node) {
+		int size = 0;
+		if (node == null) {
+			return size;
+		}
+		Queue<BinaryTreeNode> llQueue = new LinkedList<>();
+		llQueue.offer(node);
+		while (!llQueue.isEmpty()) {
+			BinaryTreeNode curr = llQueue.poll();
+			size++;
+			if (curr.left != null) {
+				llQueue.offer(curr.left);
+			}
+			if (curr.right != null) {
+				llQueue.offer(curr.right);
+			}
+		}
+		return size;
+	}
+	
+	public static int heightViaRecursion(BinaryTreeNode node) {
+		if (node == null) {
+			return 0;
+		}
+		int lHeight = heightViaRecursion(node.left);
+		int rHeight = heightViaRecursion(node.right);
+		return Math.max(lHeight, rHeight) + 1;
+	}
+	
+	public static int heightViaIteration(BinaryTreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		Stack<BinaryTreeNode> s = new Stack<>();
+		s.push(root);
+		int height = 0;
+		BinaryTreeNode prev = null;
+		while (!s.isEmpty()) {
+			BinaryTreeNode curr = s.pop();
+			if (prev == null || prev.left == curr || prev.right == curr) {
+				if (curr.left != null) {
+					s.push(curr.left);
+				}
+				if (curr.right != null) {
+					s.push(curr.right);
+				}
+			} else if (curr.left == prev) {
+				if (curr.right != null) {
+					s.push(curr.right);
+				}
+			} else {
+				s.pop();
+			}
+			height = Math.max(s.size(), height);
+		}
+		return height;
+	}
+	public static int minimumHeightViaRecursion(BinaryTreeNode root) {
+		return minimumHeight(root) - 1;
+	}
+	public static int minimumHeight(BinaryTreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int lHeight = minimumHeight(root.left);
+		int rHeight = minimumHeight(root.right);
+		return Math.min(lHeight, rHeight) + 1;
+	}
+	
+	public static int minimumHeightViaIteration(BinaryTreeNode root) {
+		int height = 0;
+		if (root != null) {
+			Queue<BinaryTreeNode> queue = new LinkedList<>();
+			
+			
+		}
+		return height;
+	}
+	
 
 }
